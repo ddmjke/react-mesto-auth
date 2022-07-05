b57122a942c5c43e4bd286bab184053082601ea1
Dmitry Konychev <dalexeych@gmail.com>
2022-07-06 00:55:05 +0300

    ref: app.js refactored to func component, be damnd r18 rd6

--- src/components/App.js
+++ src/components/App.js
@@ -9,127 +9,124 @@ import { CurrentUserContext } from '../contexts/CurrentUserContext';
 import EditProfilePopup from './EditProfilePopup';
 import EditAvatarPopup from './EditAvatarPopup';
 import AddPlacePopup from './AddPlacePopup';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
 import ProtectedRoute from './ProtectedRoute';
 import nomoAuth from '../utils/Auth';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isEditProfilePopupOpen: false,
-      isAddPlacePopupOpen: false,
-      isEditAvatarPopupOpen: false,
-      isImagePopupOpen: false,
-      isDeletePopupOpen: false,
-      cardToDelete: null,
-      selectedCard: null,
-      currentUser: null,
-      cards: [],
-      loggedIn: false,
-    }
-  }
+export default function App(props) {
+  const navigate = useNavigate();
+
+  const  [isEditProfilePopupOpen,setIsEditProfilePopupOpen]= React.useState(false);
+  const  [isAddPlacePopupOpen,setIsAddPlacePopupOpen]= React.useState(false);
+  const  [isEditAvatarPopupOpen,setIsEditAvatarPopupOpen]= React.useState(false);
+  const  [isImagePopupOpen,setIsImagePopupOpen]= React.useState(false);
+  const  [isDeletePopupOpen,setIsDeletePopupOpen]= React.useState(false);
   
-  componentDidMount() {
-    this.checkToken();
-    mestoApi.getUser()
-      .then(user => {
-        this.setState({currentUser: user})
-      })
-      .catch(err => console.log(`Failed to load initial user : ${err}`));
-    
-    mestoApi.getCards()
-      .then (cards => {
-        this.setState({cards: cards});
-      })
-     .catch(err => console.log(`Failed to load initial cards : ${err}`));
-  }
+  const [cardToDelete, setCardToDelete] = React.useState(null);
+  const [selectedCard, setSelectedCard] = React.useState(null);
+  const [currentUser, setCurrenUser] = React.useState(null);
+  const [cards, setCards] = React.useState([]);
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [autofill, setAutofill] = React.useState({password: '', email: ''});
   
-  checkToken = () => {
-    return nomoAuth.validate({token: localStorage.getItem('token')})
+  React.useEffect(
+    () => {
+      checkToken();
+      mestoApi.getUser()
+        .then(user => {
+          setCurrenUser(user);
+        })
+        .catch(err => console.log(`Failed to load initial user : ${err}`));
+      
+      mestoApi.getCards()
+        .then (cards => {
+          setCards(cards);
+        })
+        .catch(err => console.log(`Failed to load initial cards : ${err}`));
+    },
+    []
+  );
+  
+  const checkToken = () => {
+      return nomoAuth.validate({token: localStorage.getItem('token')})
       .then(res => {
         localStorage.setItem('email', res.email);
-        this.setState({loggedIn: true});
-        <Navigate to="/"/>
+        setLoggedIn(true);
+        navigate('/');
       });
   }
 
-  handleRegister = (args) => {
+  const handleRegister = (args) => {
     return nomoAuth.register(args)
       .then(res => {
         localStorage.setItem('email', res.data.email);
-        this.setState({password: args.password, email: args.email})
+        setAutofill({password: args.password, email: args.email});
         return Promise.resolve(res);
       });
   }
 
-  handleLogin = (args) => {
+  const handleLogin = (args) => {
     return nomoAuth.authorize(args)
       .then(res => {
         localStorage.setItem('email', args.email);
         localStorage.setItem('token', res.token);
-        this.setState({
-          loggedIn: true,
+        setLoggedIn(true);
+        setAutofill({
           email: args.email,
           password: args.password,
         });
       });
     }
     
-  handleLogout = () => {
+  const handleLogout = () => {
     localStorage.setItem('email', '');
     localStorage.setItem('token', '');
-    this.setState({loggedIn: false, })
+    setLoggedIn(false);
   }
 
-  handleEditProfileClick = () => {
-    this.setState({isEditProfilePopupOpen: true});
+  const handleEditProfileClick = () => {
+    setIsEditProfilePopupOpen(true);
   }
 
-  handleEditAvatarClick = () => {
-    this.setState({isEditAvatarPopupOpen: true});
+  const handleEditAvatarClick = () => {
+    setIsEditAvatarPopupOpen(true);
   }
   
-  handleAddPlaceClick = () => {
-    this.setState({isAddPlacePopupOpen: true});
+  const handleAddPlaceClick = () => {
+    setIsAddPlacePopupOpen(true);
   }
 
-  handleCardClick = (card) => {
-    this.setState({
-      isImagePopupOpen: true,
-      selectedCard: card
-    });
+  const handleCardClick = (card) => {
+    setSelectedCard(card);
+    setIsImagePopupOpen(true);
   }
 
-  handleLikeClick = (card) => {
-    const isLiked = card.likes.some(like => like._id === this.state.currentUser.id);
+  const handleLikeClick = (card) => {
+    const isLiked = card.likes.some(like => like._id === currentUser.id);
     mestoApi.toggleLike(card._id, isLiked)
       .then (res =>{
-        const newCards = this.state.cards.map(card => {return card._id === res._id ? res: card});
-        this.setState({cards: newCards});
+        const newCards = cards.map(card => {return card._id === res._id ? res: card});
+        setCards(newCards);
       })
       .catch(err => console.log(`Failed to change like status : ${err}`))
   }
 
-  handleDeleteClick = (card) => {
-    this.closeAllPopups();
-    this.setState({
-      isDeletePopupOpen: true,
-      cardToDelete: card,
-    });
+  const handleDeleteClick = (card) => {
+    closeAllPopups();
+    setCardToDelete(card);
+    setSelectedCard(card);
+    setIsDeletePopupOpen(true);
   }
 
-  handleDeleteSubmit = (evt) => {
+  const handleDeleteSubmit = (evt) => {
     evt.preventDefault();
-    return mestoApi.deleteCard(this.state.cardToDelete._id)
+    return mestoApi.deleteCard(cardToDelete._id)
       .then(() => {
-        this.setState({
-          cards: this.state.cards.filter(card => card._id !== this.state.cardToDelete._id),
-        });
-        this.setState({cardToDelete: null});
-        this.closeAllPopups();
+        setCards(cards.filter(card => card._id !== cardToDelete._id));
+        setCardToDelete(null);
+        closeAllPopups();
         return Promise.resolve();
       })
       .catch(err => {
@@ -137,27 +134,25 @@ export default class App extends React.Component {
       })
   }
 
-  closeAllPopups = () => {
-    this.setState({
-      isEditProfilePopupOpen: false,
-      isAddPlacePopupOpen: false,
-      isEditAvatarPopupOpen: false,
-      isImagePopupOpen: false,
-      isDeletePopupOpen: false,
-      selectedCard: null,
-    })
+  const closeAllPopups = () => {
+    setIsEditProfilePopupOpen(false);
+    setIsAddPlacePopupOpen(false);
+    setIsEditAvatarPopupOpen(false);
+    setIsImagePopupOpen(false);
+    setIsDeletePopupOpen(false);
+    setSelectedCard(null);
   }
 
-  handleUserUpdate = (user) => {
+  const handleUserUpdate = (user) => {
     return mestoApi.setUser(user)
       .then(res => {
-        this.setState({currentUser: {
+        setCurrenUser({
           'user-name': res.name,
           'user-profession': res.about,
           'user-pic': res.avatar,
           id: res._id,
-        }})
-        this.closeAllPopups();
+        });
+        closeAllPopups();
         return Promise.resolve(res);
       })
       .catch(err => {
@@ -165,13 +160,13 @@ export default class App extends React.Component {
       });
   }
 
-  handleAvatarUpdate = (link) => {
+  const handleAvatarUpdate = (link) => {
     return mestoApi.setAvatar({avatar: link})
       .then(res => {
-        const newUser = this.state.currentUser;
+        const newUser = currentUser;
         newUser[`user-pic`] = res.avatar;
-        this.setState(newUser);
-        this.closeAllPopups();
+        setCurrenUser(newUser);
+        closeAllPopups();
         return Promise.resolve(res);
       })
       .catch(err => {
@@ -179,75 +174,74 @@ export default class App extends React.Component {
       });
   }
 
-  handleAddPlaceSubmit = (args) => {
+  const handleAddPlaceSubmit = (args) => {
     return mestoApi.setCard(args)
       .then(res => {
-        this.setState({cards: [res, ...this.state.cards]});
-          this.closeAllPopups();
-          return Promise.resolve(res);
+        setCards([res, ...cards]);
+        closeAllPopups();
+        return Promise.resolve(res);
       })
       .catch(err => {
         return Promise.reject(`Failed to upload new place : ${err}`);
       });
   }
 
-  render() {
   return (
     <>
-      <CurrentUserContext.Provider value={this.state.currentUser || ''}>
+      <CurrentUserContext.Provider value={currentUser || ''}>
 
         <Routes>
           <Route path="/sign-in" element={
             <>
-              <Header loggedIn={this.state.loggedIn} linkTitle="Регистрация" link="/sign-up" />
-              <Login onSubmit={this.handleLogin} password={this.state.password} email={this.state.email} />
+              <Header loggedIn={loggedIn} linkTitle="Регистрация" link="/sign-up" />
+              <Login onSubmit={handleLogin} password={autofill.password} email={autofill.email} />
             </>
           }/>
 
           <Route path="/sign-up" element={
             <>
-              <Header loggedIn={this.state.loggedIn} linkTitle="Войти" link="/sign-in"/>
-              <Register onSubmit={this.handleRegister}/>
+              <Header loggedIn={loggedIn} linkTitle="Войти" link="/sign-in"/>
+              <Register onSubmit={handleRegister}/>
             </>
           }/>
 
           <Route path="/" element={
             <>
-              {this.state.loggedIn && <Header loggedIn={this.state.loggedIn} email={localStorage.getItem('email')} onLogout={this.handleLogout}/>}
+              {loggedIn && <Header loggedIn={loggedIn} email={localStorage.getItem('email')} onLogout={handleLogout}/>}
               <ProtectedRoute 
-                loggedIn={this.state.loggedIn}
+                loggedIn={loggedIn}
 
                 component={Main}
-                onEditProfile={this.handleEditProfileClick}
-                onAddPlace={this.handleAddPlaceClick}
-                onEditAvatar={this.handleEditAvatarClick}
-                onCardClick={this.handleCardClick}
-                cards={this.state.cards}
-                onCardLike={this.handleLikeClick}
-                onCardDelete={this.handleDeleteClick}
+                onEditProfile={handleEditProfileClick}
+                onAddPlace={handleAddPlaceClick}
+                onEditAvatar={handleEditAvatarClick}
+                onCardClick={handleCardClick}
+                cards={cards}
+                onCardLike={handleLikeClick}
+                onCardDelete={handleDeleteClick}
               />
             </>
           }/>
         </Routes>
         
-        {this.state.loggedIn && <Footer />}
+        {loggedIn && <Footer />}
 
         <EditProfilePopup
-          isOpen={this.state.isEditProfilePopupOpen}
-          onClose={this.closeAllPopups}
-          onUserUpdate={this.handleUserUpdate}
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+          onUserUpdate={handleUserUpdate}
         />
 
         <EditAvatarPopup
-          isOpen={this.state.isEditAvatarPopupOpen}
-          onClose={this.closeAllPopups}
-          onAvatarUpdate={this.handleAvatarUpdate}  
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+          onAvatarUpdate={handleAvatarUpdate}  
         />
 
         <AddPlacePopup 
-          isOpen={this.state.isAddPlacePopupOpen}
-          onClose={this.closeAllPopups}
-          onSubmit={this.handleAddPlaceSubmit}
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+          onSubmit={handleAddPlaceSubmit}
         />
 
         <PopupWithForm 
@@ -255,15 +249,15 @@ export default class App extends React.Component {
           title="Вы уверены?"
           buttonText="Да"
           formName="user-confirm"
-          isOpen={this.state.isDeletePopupOpen}
-          onClose={this.closeAllPopups}
-          onSubmit={this.handleDeleteSubmit}
+          isOpen={isDeletePopupOpen}
+          onClose={closeAllPopups}
+          onSubmit={handleDeleteSubmit}
           isChanged={true}
         >
         </PopupWithForm>
 
-        <ImagePopup card={this.state.selectedCard} onClose={this.closeAllPopups} isOpen={this.state.isImagePopupOpen}/>
+        <ImagePopup card={selectedCard} onClose={closeAllPopups} isOpen={isImagePopupOpen}/>
       </CurrentUserContext.Provider>
-    </>)
-  }
+    </>
+  )
 }
