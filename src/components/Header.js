3f74d641731beb19985d5bbb5bbf71c2317b1bce
Dmitry Konychev <dalexeych@gmail.com>
2022-07-06 05:15:28 +0300

    feat: sad excuse for burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,14 @@ export default function Header(props) {
       <img className="header__logo" src={logo} alt="Логотип Место" />
       {
         props.loggedIn 
-          ? <>
+          ? 
+          <>
+            <input type="checkbox" className="header__humburger-input" id="burger"/>
+            <label className="header__humburger" for="burger">
               <h2 className="header__email">{props.email}</h2>
               <NavLink className="header__logout" to="/sign-in" onClick={props.onLogout}>Выйти</NavLink>
-            </>
+            </label>
+          </>
           : <NavLink className="header__link" to={props.link}>{props.linkTitle}</NavLink>
       }
     </header>
